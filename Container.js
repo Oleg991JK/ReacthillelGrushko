7130974e3a08efd9cf58a67680453bb6e5f507cc
Oleg991JK <108993918+Oleg991JK@users.noreply.github.com>
2023-05-12 19:00:45 +0300

    Delete Container.js

--- Container.js
+++
@@ -1,9 +0,0 @@
-export function Container () {
-    const content = 'Content Here'
-
-    return(
-        <div className="container">
-            <p className="content">{content}</p>
-        </div>
-    )
-}
